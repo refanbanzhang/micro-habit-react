acf595e405d7fe40cb09db4a4164e8b13e180b03
tomcat <gaoqiang19514@163.com>
2024-01-16 18:17:13 +0800

    feat: 显示完成度

--- src/pages/index/task/index.js
+++ src/pages/index/task/index.js
@@ -1,38 +1,63 @@
 import { useState, useEffect } from 'react';
-import { Card, Typography } from '@douyinfe/semi-ui';
-import * as taskApi from '@/apis/task'
+import { Card } from '@douyinfe/semi-ui';
+import * as taskApi from '@/apis/task';
+import * as recordApi from '@/apis/record';
 
-import styles from './style.less'
-const { Text } = Typography;
+import styles from './style.less';
+
+function getPercent(obj) {
+  const percent = obj.value / obj.target;
+  return (percent * 100).toFixed(2);
+}
 
 function Task() {
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    taskApi.list().then(res => {
-      setTasks(res.data)
-    })
-  }, [])
+    taskApi.list().then((res) => {
+      setTasks(res.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!tasks.length) {
+      return;
+    }
+
+    const pros = tasks.map((item) =>
+      recordApi.list({
+        date: '2024-01-16',
+        name: item.name,
+      }),
+    );
+    Promise.all(pros).then((reses) => {
+      const records = reses.flatMap((res) => res.data);
+      const nextTasks = tasks.map((task) => {
+        const date = records.find((date) => task.name === date.name);
+        return {
+          ...task,
+          value: date?.value ?? 0,
+        };
+      });
+
+      setTasks(nextTasks);
+    });
+  }, [tasks]);
 
   return (
     <div className={styles.container}>
-      {
-        tasks.map(item => (
-          <Card
-            key={item._id}
-            title={item.name}
-            className={styles.card}
-            style={{ maxWidth: '50%' }}
-            headerExtraContent={
-              <Text link>
-                更多
-              </Text>
-            }
-          >
-            Semi Design 是由互娱社区前端团队与 UED 团队共同设计开发并维护的设计系统。设计系统包含设计语言以及一整套可复用的前端组件，帮助设计师与开发者更容易地打造高质量的、用户体验一致的、符合设计规范的 Web 应用。
-          </Card>
-        ))
-      }
+      {tasks.map((item) => (
+        <Card
+          key={item._id}
+          title={item.name}
+          className={styles.card}
+          style={{ maxWidth: '50%' }}
+        >
+          <div>目标：{item.target}</div>
+          <div>已完成：{item.value}</div>
+          <div>进度：{getPercent(item)}</div>
+        </Card>
+      ))}
     </div>
   );
 }
