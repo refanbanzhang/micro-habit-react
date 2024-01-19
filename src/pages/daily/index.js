ce6bd8527750bfc44eccbb801c24e0890edf8c3d
tomcat <gaoqiang19514@163.com>
2024-01-19 15:07:36 +0800

    feat: 完成添加任务功能

--- src/pages/daily/index.js
+++ src/pages/daily/index.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { CheckboxGroup, Checkbox } from '@douyinfe/semi-ui';
+import { Input, Button, CheckboxGroup, Checkbox, Modal } from '@douyinfe/semi-ui';
 import * as dailyTaskApi from '@/apis/dailyTask';
 import * as dailyDateApi from '@/apis/dailyDate';
 import Head from '@/shared/components/Head';
@@ -7,6 +7,8 @@ import Head from '@/shared/components/Head';
 import styles from './style.less';
 
 function Daily() {
+  const [taskNameModal, setTaskNameModal] = useState(false);
+  const [taskName, setTaskName] = useState('');
   const [items, setItems] = useState([]);
 
   useEffect(() => {
@@ -17,28 +19,34 @@ function Daily() {
 
   const onChange = (checkedValues) => {
     console.log('checked = ', checkedValues);
-    // 1. 收集任务名
+    // 1. 收集任务名（checkedValues里面就是任务名）
 
     // 2. 创建一条任务日期name date
     dailyDateApi.add({
       name: 'tg',
-      date: '2024-01-19'
+      date: '2024-01-19',
     });
   };
 
   const onAddTask = () => {
-    // 1. 收集任务名称
+    setTaskNameModal(true);
+  };
 
-    // 2. 创建任务name
-    dailyTaskApi.add({
-      name: 'tg2'
-    })
-  }
+  const onTaskNameModalOk = async () => {
+    await dailyTaskApi.add({
+      name: taskName,
+    });
+    onTaskNameModalCancel();
+  };
+
+  const onTaskNameModalCancel = () => {
+    setTaskNameModal(false);
+  };
 
   return (
     <div className={styles.container}>
       <Head />
-
+      <Button onClick={onAddTask}>创建打卡任务</Button>
       <CheckboxGroup
         style={{ width: '100%' }}
         onChange={onChange}
@@ -52,6 +60,19 @@ function Daily() {
           </Checkbox>
         ))}
       </CheckboxGroup>
+
+      <Modal
+        title="请输入任务名"
+        visible={taskNameModal}
+        onOk={onTaskNameModalOk}
+        onCancel={onTaskNameModalCancel}
+        closeOnEsc={true}
+      >
+        <Input
+          value={taskName}
+          onChange={setTaskName}
+        ></Input>
+      </Modal>
     </div>
   );
 }
