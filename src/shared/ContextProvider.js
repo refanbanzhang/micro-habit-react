19e5268cf6a0c35e90766532fdb835c9b3b79905
tomcat <gaoqiang19514@163.com>
2024-02-16 11:39:46 +0800

    refactor: 设置默认模式

--- src/shared/ContextProvider.js
+++ src/shared/ContextProvider.js
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react';
 import ThemeContext from '@/shared/ThemeContext';
 
+const CONTEXT_KEY = 'context_key';
+const DEFAULT_MODE = 'light';
+
 function ContextProvider(props) {
-  const initialContext = localStorage.getItem('context');
+  const initialContext = localStorage.getItem(CONTEXT_KEY) ?? DEFAULT_MODE;
   const [state, setState] = useState(initialContext);
 
   useEffect(() => {
-    localStorage.setItem('context', state);
+    localStorage.setItem(CONTEXT_KEY, state);
   }, [state]);
 
   return (
