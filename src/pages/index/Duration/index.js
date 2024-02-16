ba0863bdd62ccab865f21a35ac3eb81b709b28db
tomcat <gaoqiang19514@163.com>
2024-02-16 10:50:33 +0800

    refactor: 代码优化

--- src/pages/index/Duration/index.js
+++ src/pages/index/Duration/index.js
@@ -9,43 +9,39 @@ import styles from './style.less';
 
 function Duration(props) {
   const { timestamp } = props;
-  const [loading, setLoading] = useState(true);
-  const [items, setItems] = useState([]);
   const context = useContext(ThemeContext);
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
-      setLoading(true)
-      const res = await recordApi.list()
+      setLoading(true);
+      const res = await recordApi.list();
       const data = res.data ?? [];
-      setItems(data)
-      setLoading(false)
-    }
+      setItems(data);
+      setLoading(false);
+    };
     fetchData();
-  }, [timestamp])
+  }, [timestamp]);
 
   // 数据按日期归类
   const obj = items.reduce((acc, curr) => {
     if (acc[curr.date]) {
-      acc[curr.date].push(curr)
+      acc[curr.date].push(curr);
     } else {
-      acc[curr.date] = [curr]
+      acc[curr.date] = [curr];
     }
     return acc;
-  }, {})
+  }, {});
 
   // 计算每天的总时长
-  const values = Object.keys(obj).map((key) => {
-    return {
-      name: key,
-      value: obj[key].reduce((acc, curr) => {
-        return acc += curr.value
-      }, 0)
-    }
-  })
+  const values = Object.entries(obj).map(([key, values]) => ({
+    name: key,
+    value: values.reduce((acc, curr) => (acc += curr.value), 0),
+  }));
 
-  // 排序得到top3
-  const nextValues = values.sort((prev, next) => next.value - prev.value).slice(0, 3)
+  // 排序并提取top3
+  const nextValues = values.sort((prev, next) => next.value - prev.value).slice(0, 3);
 
   if (loading) {
     return <Spin indicator={<IconLoading />} />;
@@ -54,9 +50,11 @@ function Duration(props) {
   return (
     <div className={classnames([styles.container, styles[context.state]])}>
       <div>时长top3</div>
-      {
-        nextValues.map(item => <div key={item.name}>{item.name}: {item.value}</div>)
-      }
+      {nextValues.map((item) => (
+        <div key={item.name}>
+          {item.name}: {item.value}
+        </div>
+      ))}
     </div>
   );
 }
