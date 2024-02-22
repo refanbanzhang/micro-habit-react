c35a79f9a93e60869790f1859da4687cee021983
tomcat <gaoqiang19514@163.com>
2024-02-22 09:19:06 +0800

    refactor: 添加注释

--- config-overrides.js
+++ config-overrides.js
@@ -2,6 +2,9 @@ const path = require('path');
 const addLessLoader = require('customize-cra-less-loader');
 const { override, addWebpackAlias } = require('customize-cra');
 
+// 为什么不用craco呢？
+// 是因为craco在使用tailwindcss时有问题
+
 module.exports = override(
   addLessLoader({
     cssLoaderOptions: {
