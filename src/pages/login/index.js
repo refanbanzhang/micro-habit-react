d100a74172eedbace7bbb73a19f95065b077c8ee
tomcat <gaoqiang19514@163.com>
2024-02-24 14:54:13 +0800

    feat: 添加全局回车事件监听，并且提交登录

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState, useCallback } from 'react';
 import { Button, Input, Typography, Toast } from '@douyinfe/semi-ui';
 import * as userApi from '@/apis/user';
 import router from '@/router';
@@ -13,23 +13,40 @@ function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const onSubmit = async () => {
-    setLoading(true);
-    const res = await userApi.login({ username, password });
-    setLoading(false);
+  const onSubmit = useCallback(() => {
+    const request = async () => {
+      setLoading(true);
+      const res = await userApi.login({ username, password });
+      setLoading(false);
 
-    if (res.code !== 0) {
-      Toast.error('账号或密码错误');
-      return;
+      if (res.code !== 0) {
+        Toast.error('账号或密码错误');
+        return;
+      }
+
+      if (res.code === 0) {
+        localStorage.setItem('token', res.data);
+        // TODO: 接入token后，需要去掉username
+        localStorage.setItem('username', username);
+        router.navigate('/');
+      }
     }
 
-    if (res.code === 0) {
-      localStorage.setItem('token', res.data);
-      // TODO: 接入token后，需要去掉username
-      localStorage.setItem('username', username);
-      router.navigate('/');
+    request();
+  }, [username, password]);
+
+  useEffect(() => {
+    const onKeyup = (e) => {
+      if (e.key === 'Enter') {
+        onSubmit();
+      }
     }
-  };
+    window.addEventListener('keyup', onKeyup)
+
+    return () => {
+      window.removeEventListener('keyup', onKeyup)
+    }
+  }, [onSubmit])
 
   useEffect(() => {
     if (inputRef.current) {
