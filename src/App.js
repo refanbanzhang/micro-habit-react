178f8186d25803e3b64f3dad4ed0981985ce245d
refanbanzhang <gaoqiang19514@163.com>
2024-03-21 07:40:58 +0800

    feat: 接入tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import { RouterProvider } from 'react-router-dom';
-import classNames from 'classnames';
-import router from '@/router';
-import useThemeContext from '@/shared/hooks/useThemeContext';
+import { RouterProvider } from "react-router-dom";
+import classNames from "classnames";
+import router from "@/router";
+import useThemeContext from "@/shared/hooks/useThemeContext";
 
-import styles from './App.module.less';
+import styles from "./App.module.less";
 
 function App() {
   const themeContext = useThemeContext();
-  const themeClassName = themeContext.state === 'light' ? styles.light : styles.dark;
+  const themeClassName =
+    themeContext.state === "light" ? styles.light : styles.dark;
 
   return (
-    <div className={classNames([styles.container, themeClassName])}>
+    <div
+      className={classNames([
+        themeClassName,
+        "mx-auto",
+        "max-w-[500px]",
+        "min-h-[100vh]",
+      ])}
+    >
       <RouterProvider router={router} />
     </div>
   );
