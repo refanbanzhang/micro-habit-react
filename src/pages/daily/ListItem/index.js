4aa969078c8bd74c09777ca9e21d6e659e7bb83e
refanbanzhang <gaoqiang19514@163.com>
2024-03-20 14:59:44 +0800

    refactor: 调整链接位置

--- src/pages/daily/ListItem/index.js
+++ src/pages/daily/ListItem/index.js
@@ -1,4 +1,5 @@
 import { Checkbox, Dropdown } from "@douyinfe/semi-ui";
+import { IconLink } from "@douyinfe/semi-icons";
 import { IconOverflow } from "@douyinfe/semi-icons-lab";
 
 import styles from "./style.less";
@@ -16,6 +17,12 @@ function ListItem(props) {
       <div className={styles.main}>
         <span className={styles.name}>{item.name}</span>
         <div className={styles.fixed}>
+          {item.link && (
+            <IconLink
+              style={{ color: 'rgb(0, 100, 250)' }}
+              onClick={() => window.open(item.link)}
+            />
+          )}
           <Dropdown
             clickToHide
             trigger="click"
@@ -23,12 +30,6 @@ function ListItem(props) {
               <Dropdown.Menu>
                 <Dropdown.Item onClick={onRemove}>删除</Dropdown.Item>
                 <Dropdown.Item onClick={onEdit}>编辑</Dropdown.Item>
-                <Dropdown.Item
-                  disabled={!item.link}
-                  onClick={() => window.open(item.link)}
-                >
-                  跳转
-                </Dropdown.Item>
               </Dropdown.Menu>
             }
           >
