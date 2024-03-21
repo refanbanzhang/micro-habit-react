a9f2b1c9798906040e7c9b9c319ef468fea59b34
refanbanzhang <gaoqiang19514@163.com>
2024-03-21 11:44:43 +0800

    chore: 修改文件名

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ContextProvider from '@/shared/ContextProvider';
 import reportWebVitals from './reportWebVitals';
-import './index.less';
+import './index.css';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
