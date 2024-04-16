4d28ade4e37b35ace0fc9aad80ccf346fb37d3b7
refanbanzhang <gaoqiang19514@163.com>
2024-04-16 14:56:51 +0800

    chore: add ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import App from './App';
-
 import './i18n'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
