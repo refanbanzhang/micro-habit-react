8854a7bb0d23d68413023b87721a925c72699b3c
refanbanzhang <gaoqiang19514@163.com>
2024-04-17 21:43:19 +0800

    chore: setup signal

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const addLessLoader = require('customize-cra-less-loader');
-const { override, addWebpackAlias } = require('customize-cra');
+const { useBabelRc, override, addWebpackAlias } = require('customize-cra');
 
 // 为什么不用craco呢？
 // 是因为craco在使用tailwindcss时有问题
 
 module.exports = override(
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useBabelRc(),
   addLessLoader({
     cssLoaderOptions: {
       modules: true,
